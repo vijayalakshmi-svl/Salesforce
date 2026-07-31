1ed9a49eb9bca3cbf81eec711921cbdccf2f1fcd
vijayalakshmi-svl <vijayalakshmiseeram2@gmail.com>
2026-07-31 11:18:55 +0530

    Create PlacementHome.js

---
+++ LWC-Basics-Project/PlacementHome.js
@@ -0,0 +1,10 @@
+import { LightningElement } from 'lwc';
+
+export default class PlacementHome extends LightningElement {
+
+    todaysDate = '31 July 2026';
+    companies = 25;
+    jobs = 63;
+    applications = 5;
+
+}
